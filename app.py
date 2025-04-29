5ffe199c85c124ed816ae0e3f43039fe519e25bb
Jun Cheng <junelson2002@gmail.com>
2025-04-29 12:54:50 +0800

    JC add category of the listing

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ class Listing(db.Model):
     status = db.Column(db.String(50), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     image_filename = db.Column(db.String(255), nullable=True)
+    category = db.Column(db.String(100), nullable=True)
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
