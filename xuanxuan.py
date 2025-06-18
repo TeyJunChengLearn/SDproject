d5f9b4daa75b46a1fc4b361ba0a1aa6eeedee6f8
XXuannnnnn <xuanxuanteoh26@gmail.com>
2025-06-18 12:59:42 +0800

    fix: replace 'explore' with 'donation'

--- xuanxuan.py
+++ xuanxuan.py
@@ -1,2 +1,7 @@
 from flask import Blueprint,render_template,session, redirect, url_for, request as flask_request,jsonify;
+
 xuanxuan_routes = Blueprint('xuanxuan_routes', __name__)
+
+@xuanxuan_routes.route('/charity',endpoint='charity')
+def charity():
+    return render_template('charity.html')
